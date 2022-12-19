24076d850064e282d885de773efbcaca1c17056d
pawankashyapexpinfi <pawan.kashyap@expinfi.com>
2022-12-19 16:35:35 +0530

    issue fixes

--- src/rule-engine/rules/fetch.merchant.rule.ts
+++ src/rule-engine/rules/fetch.merchant.rule.ts
@@ -4,19 +4,16 @@ import { IRule } from "../interface";
 import { RuleModelRealm } from "../rule.model.realm";
 import { RuleRealm } from "../rule.realm";
 
-export class MerchantSettingRealm extends RuleRealm{}
-export class MerchantSettingModelRealm extends RuleModelRealm{}
-
 @Injectable()
-export class FetchMerchantSetting implements IRule<MerchantSettingRealm, MerchantSettingModelRealm>{
+export class FetchMerchantSetting<R extends RuleRealm, M extends RuleModelRealm> implements IRule<R, M>{
     constructor(private _zestAdapterMediator: ZestAdapterMediatorService){}
     
-    async evaluate(iRuleType: MerchantSettingRealm, iRuleDTO: MerchantSettingModelRealm) : Promise<MerchantSettingModelRealm>{
-        iRuleDTO.setMerchantSettings(await this._zestAdapterMediator.getMerchantSettings(iRuleType?.merchantId));
+    async evaluate(iRuleType: R, iRuleDTO: M) : Promise<M>{
+        iRuleDTO?.setMerchantSettings(await this._zestAdapterMediator.getMerchantSettings(iRuleType?.merchantId));
         return iRuleDTO;
     }
 
-    async shouldRun(iRuleType: MerchantSettingRealm): Promise<boolean> {
+    async shouldRun(iRuleType: R): Promise<boolean> {
         if(iRuleType?.merchantId){
             return true;
         }
